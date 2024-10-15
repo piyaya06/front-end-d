b11b72f36ac0873b56de51aa3a71bf93d4bb2f83
piyaya06 <monoapril7@gmail.com>
2024-10-15 09:56:59 +0800

    modify pertemuan 15

---
+++ Pertemuan - 15/index.js
@@ -0,0 +1,11 @@
+//Module
+import { fullName as nama, angka } from "./utils.js";
+import ucapkanSalam from "./sayGreetings.js";
+import ucapkanSalam2 from "./sayGreetings/index.js";
+import sayGreetings from "./sayGreetings.js";
+
+console.log(nama);
+console.log(angka);
+console.log(ucapkanSalam());
+console.log(ucapkanSalam2());
+console.log(sayGreetings);
